91c5d8f4d7bbb74743d64d5443d91a0e28a48dff
Zachary Behrmann <linkslayer64@gmail.com>
2018-10-12 00:33:19 -0400

    added predictable command

--- bot.py
+++ bot.py
@@ -599,8 +599,23 @@ async def drown(ctx):
 	vclient = await vchannel.connect()
 	source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("drowning.ogg"))
 	vclient.play(source)
+	
+@bot.command()
+async def predictable(ctx):
+	"""(voice) Die, yabo!"""
+	if not discord.opus.is_loaded():
+		discord.opus.load_opus('libopus.so.0')
+	if ctx.author.voice:
+		vchannel = ctx.author.voice.channel
+	else:
+		print("no channel")
+		return await ctx.send("You're not in voice Kiryu-chan!")
+	vclient = await vchannel.connect()
+	source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("predictable.ogg"))
+	vclient.play(source)
 
 #todo - add all of these to a "voicecommand" sucsection so I don't need to type out each one
+@predictable.after_invoke
 @breaker.after_invoke
 @drown.after_invoke
 @mahinapeaa.after_invoke
